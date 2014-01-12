11f5bbf5261633e93713de8410d76e6bc95465a8
System Administrator <root@Carloss-MacBook-Pro.local>
2014-01-12 14:01:06 -0500

    Commit Inicial

---
+++ js/ccJavaScript.js
@@ -0,0 +1,27 @@
+function updateSizes() {   
+
+    var width = $(window).width();
+    var height = $(window).height();
+    var headerMinH = 348;
+
+    var fixedH = height*0.6;
+    
+    if (height > 580 && width > 640) {
+        $('.mid-height').css('height', fixedH.toFixed(0));
+    } else {
+        $('.mid-height').css('height', headerMinH);
+    }
+
+    $('.full-height').css('height', height);
+
+}
+
+function scrollToElement( target ) {
+    var topoffset = 30;
+    var speed = 800;
+    var destination = $( target ).offset().top - topoffset;
+    $( 'html:not(:animated),body:not(:animated)' ).animate( { scrollTop: destination}, speed, function() {
+        //window.location.hash = target;
+    });
+    return false;
+}
